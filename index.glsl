e505ed7424ab853776271891252e24d5a4497a0a
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2016-09-26 01:12:06 -0400

    solve for both intersections

--- index.glsl
+++ index.glsl
@@ -2,14 +2,19 @@
 #define iSteps 16
 #define jSteps 8
 
-float rsi(vec3 r0, vec3 rd, float sr) {
-    // Simplified ray-sphere intersection that assumes
-    // the ray starts inside the sphere and that the
-    // sphere is centered at the origin. Always intersects.
+vec2 rsi(vec3 r0, vec3 rd, float sr) {
+    // ray-sphere intersection that assumes
+    // the sphere is centered at the origin.
+    // No intersection when result.x > result.y
     float a = dot(rd, rd);
     float b = 2.0 * dot(rd, r0);
     float c = dot(r0, r0) - (sr * sr);
-    return (-b + sqrt((b*b) - 4.0*a*c))/(2.0*a);
+    float d = (b*b) - 4.0*a*c;
+    if (d < 0.0) return vec2(1e5,-1e5);
+    return vec2(
+        (-b - sqrt(d))/(2.0*a),
+        (-b + sqrt(d))/(2.0*a)
+    );
 }
 
 vec3 atmosphere(vec3 r, vec3 r0, vec3 pSun, float iSun, float rPlanet, float rAtmos, vec3 kRlh, float kMie, float shRlh, float shMie, float g) {
@@ -18,7 +23,10 @@ vec3 atmosphere(vec3 r, vec3 r0, vec3 pSun, float iSun, float rPlanet, float rAt
     r = normalize(r);
 
     // Calculate the step size of the primary ray.
-    float iStepSize = rsi(r0, r, rAtmos) / float(iSteps);
+    vec2 p = rsi(r0, r, rAtmos);
+    if (p.x > p.y) return vec3(0,0,0);
+    p.y = min(p.y, rsi(r0, r, rPlanet).x);
+    float iStepSize = (p.y - p.x) / float(iSteps);
 
     // Initialize the primary ray time.
     float iTime = 0.0;
@@ -56,7 +64,7 @@ vec3 atmosphere(vec3 r, vec3 r0, vec3 pSun, float iSun, float rPlanet, float rAt
         iOdMie += odStepMie;
 
         // Calculate the step size of the secondary ray.
-        float jStepSize = rsi(iPos, pSun, rAtmos) / float(jSteps);
+        float jStepSize = rsi(iPos, pSun, rAtmos).y / float(jSteps);
 
         // Initialize the secondary ray time.
         float jTime = 0.0;
